919f8256abf51c67725f52cd4abb948eb6a9a199
camnh <camnh@MacBook-Pro-cua-camnh.local>
2026-03-31 15:07:33 +0700

    Adding class

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,13 +1,13 @@
 const Footer = () => {
   return (
     <footer
-      class="text-white pt-4 pb-4 footer"
+      className="text-white pt-4 pb-4 footer"
       style={{ background: "#e60012" }}
     >
-      <div class="container text-md-start">
-        <div class="row align-items-center">
-          <div class="col-md-7 col-lg-8">
-            <p class="text-center text-md-start mb-0">
+      <div className="container text-md-start">
+        <div className="row align-items-center">
+          <div className="col-md-7 col-lg-8">
+            <p className="text-center text-md-start mb-0">
               <strong>Designed and implemented by: Camnh</strong>
             </p>
           </div>
